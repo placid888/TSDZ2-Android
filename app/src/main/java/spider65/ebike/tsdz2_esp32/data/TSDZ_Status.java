86b2da9b3779382da635db98e0f3e490272d306f
placid888 <placid888@gmail.com>
2026-09-08 15:08:30 +0800

    update TSDZ_Status.java

--- app/src/main/java/spider65/ebike/tsdz2_esp32/data/TSDZ_Status.java
+++ app/src/main/java/spider65/ebike/tsdz2_esp32/data/TSDZ_Status.java
@@ -4,13 +4,7 @@ import android.util.Log;
 
 import static spider65.ebike.tsdz2_esp32.TSDZConst.STATUS_ADV_SIZE;
 
-// Status Characteristic Notification
-// According to the BLE specification the notification length can be max ATT_MTU - 3.
-// The 3 bytes subtracted is the 3-byte header(OP-code (operation, 1 byte) and the attribute handle (2 bytes))
-// In BLE 4.1 the ATT_MTU is 23 bytes, but in BLE 4.2 the ATT_MTU can be negotiated up to 247 bytes
-// -> Max payload for BT 4.1 is 20 bytes
 public class TSDZ_Status {
-
     private static final String TAG = "TSDZ_Status";
 
     public RidingMode ridingMode;
@@ -30,19 +24,17 @@ public class TSDZ_Status {
     public int wattHour;
     public short rxcErrors;
     public short rxlErrors;
-
     public short torqueSmoothPct;
     public short torqueSmoothAvg;
     public short torqueSmoothMin;
     public short torqueSmoothMax;
-
     public short dutyCycle;
     public int motorERPS;
     public short focAngle;
-    public int torqueADCValue; // Torque sensor ADC value (16 bits)
-    public short adcThrottle; // value from ADC Throttle/Temperature
-    public short throttle;    // Throttled mapped to 0-255
-    public float pTorque;     // Torque in Nm
+    public int torqueADCValue;
+    public short adcThrottle;
+    public short throttle;
+    public float pTorque;
     public short fwOffset;
     public float pcbTemperature;
     public boolean timeDebug;
@@ -53,7 +45,7 @@ public class TSDZ_Status {
     public short debug4;
     public short debug5;
     public short debug6;
-
+    public short soc;
 
     public enum RidingMode {
         OFF_MODE(0),
@@ -77,48 +69,12 @@ public class TSDZ_Status {
         }
     }
 
-    /*
-    #pragma pack(1)
-    typedef struct _tsdz_data {
-        volatile uint8_t ui8_riding_mode;               // 0 - bit 7 Street Mode enabled flag
-        volatile uint8_t ui8_assist_level;              // 1
-        volatile uint8_t ui8_system_state;              // 2
-        volatile uint16_t ui16_wheel_speed_x10;         // 3
-        volatile uint8_t ui8_pedal_cadence_RPM;         // 5
-        volatile uint16_t ui16_pedal_torque_x100;       // 6
-        volatile int16_t i16_motor_temperaturex10;      // 8
-        volatile int16_t i16_pcb_temperaturex10;        // 10
-        volatile uint16_t ui16_battery_voltage_x1000;   // 12
-        volatile uint8_t ui8_battery_current_x10;       // 14
-        volatile uint16_t ui16_battery_wh;              // 15
-        volatile uint8_t ui8_adc_throttle;              // 17
-        volatile uint8_t ui8_throttle;                  // 18
-        volatile uint16_t ui16_adc_pedal_torque_sensor; // 19
-        volatile uint8_t ui8_duty_cycle;                // 21
-        volatile uint16_t ui16_motor_speed_erps;        // 22
-        volatile uint8_t ui8_foc_angle;                 // 24
-        volatile uint8_t ui8_fw_hall_cnt_offset;        // 25
-        volatile uint8_t ui8_TorqueSmoothPct;           // 26
-        volatile uint8_t ui8_TorqueAVG;                 // 27
-        volatile uint8_t ui8_TorqueMin;                 // 28
-        volatile uint8_t ui8_TorqueMax;                 // 29
-        volatile uint8_t ui8_rxc_errors;                // 30
-        volatile uint8_t ui8_rxl_errors;                // 31
-        volatile uint8_t ui8_debugFlags;                // 32
-        volatile uint8_t ui8_debug1;                    // 33
-        volatile uint8_t ui8_debug2;                    // 34
-        volatile uint8_t ui8_debug3;                    // 35
-        volatile uint8_t ui8_debug4;                    // 36
-        volatile uint8_t ui8_debug5;                    // 37
-        volatile uint8_t ui8_debug6;                    // 38
-    } struct_tsdz_data;
-    */
-
     public boolean setData(byte[] data) {
         if (data.length != STATUS_ADV_SIZE) {
             Log.e(TAG, "Wrong Status BT message size!");
             return false;
         }
+
         ridingMode = RidingMode.valueOf(data[0] & 0x7f);
         streetMode = (data[0] & 0x80) != 0;
         assistLevel = (short)(data[1] & 255);
@@ -127,16 +83,19 @@ public class TSDZ_Status {
         controllerFromESP32ReceiveError = (data[2] & 0x10) != 0;
         esp32FromControllerReceiveError = (data[2] & 0x80) != 0;
         esp32FromLDCReceiveError = (data[2] & 0x40) != 0;
+
         speed = (float)(((data[4] & 255) << 8) + (data[3] & 255)) / 10;
         cadence = (short)(data[5] & 255);
+
         long l = ((data[7] & 255) << 8) + (data[6] & 255);
         pTorque = (float)l / 100;
         pPower = (int)((l * cadence / 96) + 5) / 10;
-        // temperature is a signed short
+
         short t = (short)(((data[9] & 0xff) << 8) | (data[8] & 0xff));
         motorTemperature = (float)(t) / 10;
         t = (short)(((data[11] & 0xff) << 8) | (data[10] & 0xff));
         pcbTemperature = (float)(t) / 10;
+
         volts = (float)(((data[13] & 255) << 8) + (data[12] & 255)) / 1000;
         amperes = (float)(data[14] & 255) / 10;
         wattHour = ((data[16] & 255) << 8) + ((data[15] & 255));
@@ -161,6 +120,8 @@ public class TSDZ_Status {
         debug4 = (short)(data[36] & 255);
         debug5 = (short)(data[37] & 255);
         debug6 = (short)(data[38] & 255);
+        soc = (short)(data[38] & 255); 
+        
         return true;
     }
 }
